2d6a785009eb968bdc2285625645ad2a2570d57d
SOWBARNIKA05 <sowpriya0509@gmail.com>
2025-05-29 09:27:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Simple script to add smooth scroll behavior
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
